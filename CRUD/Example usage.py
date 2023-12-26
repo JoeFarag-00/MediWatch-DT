c2582a0d3e1cc92c083bff74d14ffedecc3d5993
Youssef Raghed Farag <88057098+JoeFarag-00@users.noreply.github.com>
2023-12-26 17:37:22 +0200

    Delete CRUD directory

--- CRUD/Example usage.py
+++
@@ -1,7 +0,0 @@
-from Doctor import Doctor
-from Nurse import Nurse
-from Patient import Patient
-from bson.objectid import ObjectId # !!!!!!!!!!!!!!!!! This is import of ObjectId() may be need for adding a nurse to the nurse class !!!!!!!!!!!!!!!!
-
-# You will find each class example usage in each file
-# HAVE FUN :) !!!!!!!!
